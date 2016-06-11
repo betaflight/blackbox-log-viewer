28c233fff5c18b3e4d8a9e89cb2f95a65bb54b65
Gary Keeble <garykeeble@garys-mbp.home>
2016-06-11 22:29:15 +0100

    Display Frequency on Marker Time
    Show calculated Hz for marker offset

--- js/main.js
+++ js/main.js
@@ -185,7 +185,7 @@ function BlackboxLogViewer() {
             // update time field on status bar
             $(".graph-time").val(formatTime((currentBlackboxTime-flightLog.getMinTime())/1000, true));
             if(hasMarker) {
-                $("#status-bar .marker-offset").text('Marker Offset ' + formatTime((currentBlackboxTime-markerTime)/1000, true) + 'ms');
+                $("#status-bar .marker-offset").text('Marker Offset ' + formatTime((currentBlackboxTime-markerTime)/1000, true) + 'ms ' + (1000000/(currentBlackboxTime-markerTime)).toFixed(0) + "Hz");
             }
 
             
