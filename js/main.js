081269f26917b7d675bc4684c945d6c841e4cffe
Gary Keeble <garykeeble@garys-mbp.home>
2016-06-11 18:04:59 +0100

    Custom Mixes
    Add User Settings Dialog to allow custom mixes

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 "use strict";
 
+// Global Level Variables
+var userSettings = {};
+
 function BlackboxLogViewer() {
     function supportsRequiredAPIs() {
         return window.File && window.FileReader && window.FileList && Modernizr.canvas;
@@ -41,6 +44,7 @@ function BlackboxLogViewer() {
         
         // JSON graph configuration:
         graphConfig = {},
+        
 
         offsetCache = [], // Storage for the offset cache (last 20 files)
         currentOffsetCache = {log:null, index:null, video:null, offset:null},
@@ -698,6 +702,16 @@ function BlackboxLogViewer() {
             graphConfig = GraphConfig.getExampleGraphConfigs(flightLog, ["Motors", "Gyros"]);
         }
     });
+    
+    prefs.get('userSettings', function(item) {
+		if(item) {
+	    			userSettings = item;
+				 } else {
+					 userSettings = { // default settings
+							 		customMix: null
+					 	 			};
+				 }
+    	});
 
     // Workspace save/restore to/from file.
     function saveWorkspaces(file) {
@@ -1070,6 +1084,19 @@ function BlackboxLogViewer() {
             }),
 
             keysDialog = new KeysDialog($("#dlgKeysDialog")),
+            
+            userSettingsDialog = new UserSettingsDialog($("#dlgUserSettings"), function(newSettings) {
+	            userSettings = newSettings;
+
+	            prefs.set('userSettings', newSettings);
+
+	            // refresh the craft model
+	            if(graph!=null) {
+	                graph.initializeCraftModel();
+	                invalidateGraph();
+	            }
+
+	        }),
 
 	        exportDialog = new VideoExportDialog($("#dlgVideoExport"), function(newConfig) {
 	            videoConfig = newConfig;
@@ -1094,6 +1121,12 @@ function BlackboxLogViewer() {
             e.preventDefault();
             
             keysDialog.show();
+        });
+
+        $(".open-user-settings-dialog").click(function(e) {
+            e.preventDefault();
+            
+            userSettingsDialog.show(flightLog, userSettings);
         });
 
         $("#status-bar .marker-offset").click(function(e) {
