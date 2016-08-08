12b67c2406b0211661b448797c03623f32cb7a44
Gary Keeble <garykeeble@Garys-MacBook-Pro.local>
2016-08-08 10:21:55 +0100

    Updated Version Number to 2.5.1

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
 // Global Level Variables
 var userSettings = {};
 
-var VIEWER_VERSION = '2.5.0'; // Current version
+var VIEWER_VERSION = '2.5.1'; // Current version
 
 function BlackboxLogViewer() {
     function supportsRequiredAPIs() {
