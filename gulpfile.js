7fb9c523a4c24dbd25ba5a0d4e6a6afc5e3ae714
Bas Delfos <b.delfos@gmail.com>
2018-02-18 11:35:12 +0100

    Updated gulp build (added rpm build and fixed linux build on macOS)

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,7 @@ const del = require('del');
 const NwBuilder = require('nw-builder');
 const makensis = require('makensis');
 const deb = require('gulp-debian');
+const commandExistsSync = require('command-exists').sync;
 
 const gulp = require('gulp');
 const concat = require('gulp-concat');
@@ -470,7 +471,12 @@ function compressFiles(srcPath, basePath, outputFile, zipFolder) {
                .pipe(gulp.dest(RELEASE_DIR));
 }
 
-function release_deb(arch) {
+function release_deb(arch, done) {
+    // Check if dpkg-deb exists
+    if (!commandExistsSync('dpkg-deb')) {
+        console.warn('dpkg-deb command not found, not generating deb package for ' + arch);
+        return done();
+    }
 
     var debArch;
 
@@ -505,6 +511,46 @@ function release_deb(arch) {
              _copyright: 'assets/linux/copyright',
              _clean: true
     }));
+}
+
+function release_rpm(arch, done) {
+
+    // Check if dpkg-deb exists
+    if (!commandExistsSync('rpmbuild')) {
+        console.warn('rpmbuild command not found, not generating rpm package for ' + arch);
+        return done();
+    }
+
+    // The buildRpm does not generate the folder correctly, manually
+    createDirIfNotExists(RELEASE_DIR);
+
+    var options = {
+             name: pkg.name,
+             version: pkg.version,
+             buildArch: getLinuxPackageArch('rpm', arch),
+             vendor: pkg.author,
+             summary: pkg.description,
+             license: 'GNU General Public License v3.0',
+             requires: 'libgconf-2-4',
+             prefix: '/opt',
+             files:
+                 [ { cwd: path.join(APPS_DIR, pkg.name, arch),
+                     src: '*',
+                     dest: '/opt/betaflight/blackbox-log-viewer' } ],
+             postInstallScript: ['xdg-desktop-menu install /opt/betaflight/blackbox-log-viewer/blackbox-log-viewer.desktop'],
+             preUninstallScript: ['xdg-desktop-menu uninstall blackbox-log-viewer.desktop'],
+             tempDir: path.join(RELEASE_DIR,'tmp-rpm-build-' + arch),
+             keepTemp: false,
+             verbose: false,
+             rpmDest: RELEASE_DIR
+    };
+
+    buildRpm(options, function(err, rpm) {
+        if (err) {
+          console.error("Error generating rpm package: " + err);
+        }
+        done();
+    });
 }
 
 // Create distribution package for macOS platform
@@ -564,12 +610,14 @@ function listReleaseTasks(done) {
 
     if (platforms.indexOf('linux64') !== -1) {
         releaseTasks.push(function release_linux64_zip(){ return release_zip('linux64') });
-        releaseTasks.push(function release_linux64_deb(){ return release_deb('linux64') });
+        releaseTasks.push(function release_linux64_deb(done){ return release_deb('linux64', done) });
+        releaseTasks.push(function release_linux64_rpm(done){ return release_rpm('linux64', done) });
     }
 
     if (platforms.indexOf('linux32') !== -1) {
         releaseTasks.push(function release_linux32_zip(){ return release_zip('linux32') });
-        releaseTasks.push(function release_linux32_deb(){ return release_deb('linux32') });
+        releaseTasks.push(function release_linux32_deb(done){ return release_deb('linux32', done) });
+        releaseTasks.push(function release_linux32_rpm(done){ return release_rpm('linux32', done) });
     }
 
     if (platforms.indexOf('osx64') !== -1) {
