6dc05293c22efe54ab980abd8a779605a0c3177d
Henrik Wolkesson <henrik.wolkesson@gmail.com>
2018-04-15 18:30:04 +0200

    Icon for Start log here. Adjusted Collapse and Expansion of toolbar. Hide toolbar headings on small screens.

--- js/main.js
+++ js/main.js
@@ -1857,7 +1857,7 @@ function BlackboxLogViewer() {
             .on("slide change set", function() {
                 setPlaybackRate(parseFloat($(this).val()));
             })
-            .Link("lower").to($(".playback-rate"));
+            .Link("lower").to($(".playback-rate-text"));
     
         $(".graph-zoom-control")
             .noUiSlider({
