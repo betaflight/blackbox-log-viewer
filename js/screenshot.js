c9e62141a48ff156198eeac0017f9e063174f673
Kiripolszky Károly <k.kiripolszky@mito.hu>
2019-10-20 12:23:36 +0200

    Suggest a filename on the save screenshot dialog

--- js/screenshot.js
+++ js/screenshot.js
@@ -9,11 +9,20 @@ function makeScreenshot() {
             This option ensures, that the image content won't overflow to the bottom.
             */
             scrollY: -1 * parseInt(elStyle.top),
-        };
+        },
+        now = new Date(),
+        timestamp = "" + now.getFullYear() +
+            ("00" + (now.getMonth() + 1)).slice(-2) +
+            ("00" + now.getDate()).slice(-2) + "-" +
+            ("00" + now.getHours()).slice(-2) + 
+            ("00" + now.getMinutes()).slice(-2) + 
+            ("00" + now.getSeconds()).slice(-2),
+        defaultFilename = $(".log-filename").text().replace(".", "_") + "-" 
+            + timestamp + ".png";
     html2canvas(el, options).then(canvas => {
         window.canv = canvas;
         let anchor = document.createElement("a");
-        anchor.download = "graph-screenshot.png";
+        anchor.download = defaultFilename;
         anchor.href = canvas.toDataURL();
         anchor.click();
     });
