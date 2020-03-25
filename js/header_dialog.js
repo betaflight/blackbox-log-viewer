7bc7f12e3345c9fde3ee03be8150f45d600235be
Miguel Angel Mulero Martinez <migmul@gmail.com>
2020-03-25 13:34:12 +0100

    Fix Sonar issues

--- js/header_dialog.js
+++ js/header_dialog.js
@@ -824,7 +824,7 @@ function HeaderDialog(dialog, onSave) {
             $('#dlgHeaderDialog input').prop('disabled', 'disabled');
             $('#dlgHeaderDialog select').prop('disabled', 'disabled');
         });
-        
+
         dialog.modal('toggle');
 
     }
