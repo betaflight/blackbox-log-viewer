80096c8a376409e68d32e821e455872238f2b850
demvlad <demvlad2@yandex.ru>
2024-01-29 11:13:43 +0300

    added: show context menu by right mouse click at minmax control elements in setup dialog box

--- js/graph_config_dialog.js
+++ js/graph_config_dialog.js
@@ -127,9 +127,9 @@ function GraphConfigurationDialog(dialog, onSave) {
                     + '<td><input name="linewidth" class="form-control" type="text"/></td>'
                     + '<td><select class="color-picker"></select></td>'
                     + '<td><input name="grid" type="checkbox"/></td>'
-                    + '<td><input name="EnabledMinMax" type="checkbox"/></td>'
-                    + '<td><input name="MinValue" class="form-control" type="text" readonly="true"/></td>'
-                    + '<td><input name="MaxValue" class="form-control" type="text" readonly="true"/></td>'
+                    + '<td><input name="EnabledMinMax" class="minmax-control"  type="checkbox"/></td>'
+                    + '<td><input name="MinValue" class="form-control minmax-control" type="text" readonly="true"/></td>'
+                    + '<td><input name="MaxValue" class="form-control minmax-control" type="text" readonly="true"/></td>'
                     + '<td><button type="button" class="btn btn-default btn-sm"><span class="glyphicon glyphicon-trash"></span></button></td>'
                 + '</tr>'
             ),
@@ -192,10 +192,43 @@ function GraphConfigurationDialog(dialog, onSave) {
             }
         });
         
+        $('.minmax-control', elem).contextmenu( function(e) {
+            if($('input[name=EnabledMinMax]', elem).is(':checked')) {
+                let name = field.name ?? $('select.form-control option:selected', elem).val();
+                showMinMaxSetupContextMenu(name, $(".config-graph-field", $(this).parents('.config-graph')), e);
+            }
+            return false;
+        });
         
-        
-
         return elem;
+    }
+    
+    function showMinMaxSetupContextMenu(field_name, field_list, e) {
+        let rows = [];
+        field_list.each(function() {
+            let enabled = $('input[name=EnabledMinMax]', this).is(':checked');
+            if(enabled) {
+                let fieldName = $("select", this).val();
+                let minimum = $("input[name=MinValue]", this).val();
+                let maximum = $("input[name=MaxValue]", this).val();
+
+                let row = {
+                    name: fieldName,
+                    min: parseFloat(minimum),
+                    max: parseFloat(maximum)
+                };
+                rows.push(row);
+            }
+        });
+        
+        let menu = new nw.Menu();
+        menu.append(new nw.MenuItem({label: 'Set all minmax values to default'}));
+        menu.append(new nw.MenuItem({label: 'Set all curves to one scale'}));
+        menu.append(new nw.MenuItem({type: 'separator'}));
+        menu.append(new nw.MenuItem({label: 'Set this curve minmax to default'}));
+        menu.append(new nw.MenuItem({label: 'Set this curve to one scale at:'}));
+        menu.append(new nw.MenuItem({label: 'Set this curve to one zero level at:'}));
+        menu.popup(e.screenX, e.screenY);
     }
     
     function renderGraph(flightLog, index, graph) {
@@ -359,7 +392,7 @@ function GraphConfigurationDialog(dialog, onSave) {
             graph.height = parseInt($('select.graph-height option:selected', this).val());
             
             $(".config-graph-field", this).each(function() {
-                let fieldName = $("select", this).val(); 
+                let fieldName = $("select", this).val();
                 let minimum = $("input[name=MinValue]", this).val();
                 let maximum = $("input[name=MaxValue]", this).val();
                 
