2133f2e8761479839060b61e6ebdb85fba6bc53e
demvlad <demvlad2@yandex.ru>
2024-02-15 13:31:02 +0300

    removed unusing local variable

--- js/graph_config_dialog.js
+++ js/graph_config_dialog.js
@@ -436,7 +436,6 @@ function GraphConfigurationDialog(dialog, onSave) {
 
         var ShowCurvesToSetZoomCheckboxedMenu = function (multipleCall) {
             let CurvesCheckboxedMenu = new nw.Menu();
-            const SelectedCurveName = $('select.form-control option:selected', selected_curve).text();
             for (const key in curvesData) {
                 const curve = curvesData[key];
                 if (multipleCall==undefined)
