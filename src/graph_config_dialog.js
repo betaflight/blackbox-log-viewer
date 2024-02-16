f79645f5ef01bafcf2d29afdfb8159e7180302b3
demvlad <demvlad2@yandex.ru>
2024-02-16 08:02:51 +0300

    added caption to checkboxed menu

--- src/graph_config_dialog.js
+++ src/graph_config_dialog.js
@@ -309,6 +309,13 @@ export function GraphConfigurationDialog(dialog, onSave) {
 
         var ShowCurvesToSetMinMaxCheckboxedMenu = function() {
             let CurvesCheckboxedMenu = new nw.Menu();
+            CurvesCheckboxedMenu.append(new nw.MenuItem({
+                label: "SELECT CURVES:",
+                enabled: false,
+                }));
+            CurvesCheckboxedMenu.append(new nw.MenuItem({
+                    type:  'separator'
+                }));
             for (const key in curvesData) {
                 const curve = curvesData[key];
                 if (!curve.selected) {
@@ -324,7 +331,7 @@ export function GraphConfigurationDialog(dialog, onSave) {
                         type:  'separator'
                     }));
             CurvesCheckboxedMenu.append(new nw.MenuItem({
-                        label: "Set min-max values",
+                        label: "SET MIN-MAX VALUES",
                         click: ApplySelectedCurveMinMaxToOtherSelectedCurves
                     }));
 
@@ -354,6 +361,13 @@ export function GraphConfigurationDialog(dialog, onSave) {
         var ShowCurvesToSetSameScaleCheckboxedMenu = function(multipleCall) {
             let CurvesCheckboxedMenu = new nw.Menu();
             const SelectedCurveName = $('select.form-control option:selected', selected_curve).text();
+            CurvesCheckboxedMenu.append(new nw.MenuItem({
+                label: "SELECT CURVES:",
+                enabled: false,
+                }));
+            CurvesCheckboxedMenu.append(new nw.MenuItem({
+                    type:  'separator'
+                }));
             for (const key in curvesData) {
                 const curve = curvesData[key];
                 // Checked selected curve when menu is showed firstly
@@ -370,7 +384,7 @@ export function GraphConfigurationDialog(dialog, onSave) {
                         type:  'separator'
                     }));
             CurvesCheckboxedMenu.append(new nw.MenuItem({
-                        label: "Set to same scale",
+                        label: "SET CURVES TO SAME SCALE",
                         click: FitSelectedCurveToSameScale
                     }));
 
@@ -435,6 +449,14 @@ export function GraphConfigurationDialog(dialog, onSave) {
 
         var ShowCurvesToSetZoomCheckboxedMenu = function (multipleCall) {
             let CurvesCheckboxedMenu = new nw.Menu();
+            
+            CurvesCheckboxedMenu.append(new nw.MenuItem({
+                label: "SELECT CURVES:",
+                enabled: false,
+                }));
+            CurvesCheckboxedMenu.append(new nw.MenuItem({
+                    type:  'separator'
+                }));
             for (const key in curvesData) {
                 const curve = curvesData[key];
                 if (multipleCall==undefined)
@@ -450,7 +472,7 @@ export function GraphConfigurationDialog(dialog, onSave) {
                     type: 'separator'
                 }));
 
-            const Caption = zoomScale < 1 ? "Zoom in " : "Zoom out ";
+            const Caption = zoomScale < 1 ? "APPLY ZOOM IN " : "APPLY ZOOM OUT ";
             const procent =  Math.abs((1.0 - zoomScale) * 100 ).toFixed(0) + "%"
             CurvesCheckboxedMenu.append(new nw.MenuItem({
                         label: Caption + procent,
@@ -517,7 +539,6 @@ export function GraphConfigurationDialog(dialog, onSave) {
             }));
 
         let mainMenu = new nw.Menu();
-
         if (!oneRow) {
             mainMenu.append(new nw.MenuItem({
                 label: 'Set all curves min-max values to default',
@@ -579,7 +600,7 @@ export function GraphConfigurationDialog(dialog, onSave) {
         }));
         mainMenu.append(new nw.MenuItem({type: 'separator'}));
         mainMenu.append(new nw.MenuItem({
-            label: 'Zoom',
+            label: 'Curves zoom',
             submenu: zoomSubMenu
         }));
 
