8bd7695c5fc8c1858449931efd9695080145f340
demvlad <demvlad2@yandex.ru>
2024-02-25 16:40:23 +0300

    add: block outer interface while menu is opened

--- js/graph_config_dialog.js
+++ js/graph_config_dialog.js
@@ -514,8 +514,8 @@ function GraphConfigurationDialog(dialog, onSave) {
             elem = $('<div class="topBorder">APPLY ZOOM</div>');
             elem.click(function () {
                 let zoomScale = parseFloat($("input[type=number]", menu3).val());
-				zoomScale = Math.min(zoomScale, 5.0);
-				zoomScale = Math.max(zoomScale, 1000);
+                zoomScale = Math.min(zoomScale, 5.0);
+                zoomScale = Math.max(zoomScale, 1000);
                 zoomScale = 100.0/zoomScale;
                 SetZoomToSelectedCurves(zoomScale);
             });
@@ -667,9 +667,11 @@ function GraphConfigurationDialog(dialog, onSave) {
         elem.click(function () {
             menu1.removeClass('show');
             menu1.empty();
+            $('.config-graph-field').css('pointer-events', 'all'); 
         });
         menu1.append(elem);
         menu1.addClass('show');
+        $('.config-graph-field').css('pointer-events', 'none'); 
     }
 
     function renderGraph(flightLog, index, graph) {
