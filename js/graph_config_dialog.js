18ff8a2f406220412006ebb03e9d97e38b7d8f80
demvlad <demvlad2@yandex.ru>
2024-02-25 22:00:32 +0300

    resolved issue of zoom computing

--- js/graph_config_dialog.js
+++ js/graph_config_dialog.js
@@ -516,8 +516,8 @@ function GraphConfigurationDialog(dialog, onSave) {
             elem = $('<div class="topBorder">APPLY ZOOM</div>');
             elem.click(function () {
                 let zoomScale = parseFloat($("input[type=number]", menu3).val());
-                zoomScale = Math.min(zoomScale, 5.0);
-                zoomScale = Math.max(zoomScale, 1000);
+                zoomScale = Math.max(zoomScale, 5.0);
+                zoomScale = Math.min(zoomScale, 1000);
                 zoomScale = 100.0/zoomScale;
                 SetZoomToSelectedCurves(zoomScale);
             });
