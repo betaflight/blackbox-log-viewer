e33623ad5744bcffd453f821cc969dfb359c59b2
Mark Haslinghuis <mark@numloq.nl>
2024-02-26 23:15:25 +0100

    Add SCHEDULER_DETERMINISM field

--- js/flightlog_fields_presenter.js
+++ js/flightlog_fields_presenter.js
@@ -911,7 +911,7 @@ function FlightLogFieldPresenter() {
             'debug[4]':'Minimum Gyro period in 100th of a us',
             'debug[5]':'Maximum Gyro period in 100th of a us',
             'debug[6]':'Span of Gyro period in 100th of a us',
-            'debug[7]':'Not Used',
+            'debug[7]':'Gyro cycle deviation in 100th of a us',
         },
         'TIMING_ACCURACY' : {
             'debug[all]':'Timing Accuracy',
