815933d179c7619134d04e2566deb365d40d8788
nerdCopter <56646290+nerdCopter@users.noreply.github.com>
2024-03-05 10:41:43 -0600

    Update js/header_dialog.js
    Co-authored-by: Mark Haslinghuis <mark@numloq.nl>

--- js/header_dialog.js
+++ js/header_dialog.js
@@ -602,7 +602,7 @@ function HeaderDialog(dialog, onSave) {
         setParameter('rcYawExpo'                ,sysConfig.rc_expo[2],2);
         setParameter('vbatscale'				,sysConfig.vbatscale,0);
         setParameter('vbatref'					,sysConfig.vbatref,0);
-        if(activeSysConfig.firmwareType === FIRMWARE_TYPE_BETAFLIGHT && semver.gte(activeSysConfig.firmwareVersion, '4.0.0')) {
+        if (activeSysConfig.firmwareType === FIRMWARE_TYPE_BETAFLIGHT && semver.gte(activeSysConfig.firmwareVersion, '4.0.0')) {
             setParameter('vbatmincellvoltage'       ,sysConfig.vbatmincellvoltage,2);
             setParameter('vbatmaxcellvoltage'       ,sysConfig.vbatmaxcellvoltage,2);
             setParameter('vbatwarningcellvoltage'   ,sysConfig.vbatwarningcellvoltage,2);
