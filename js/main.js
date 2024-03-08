382e4330a1175475a9fecb79e7377b51a9c84e64
demvlad <demvlad2@yandex.ru>
2024-03-08 22:56:05 +0300

    resolved issue of workspace saving logic

--- js/main.js
+++ js/main.js
@@ -997,11 +997,11 @@ function BlackboxLogViewer() {
         GpxExporter(flightLog).dump(onSuccess);
     }
 
-    function newGraphConfig(newConfig) {
+    function newGraphConfig(newConfig, switchWorkspace) {
         lastGraphConfig = graphConfig; // Remember the last configuration.
         graphConfig = newConfig;
 
-        activeGraphConfig.adaptGraphs(flightLog, graphConfig);
+        activeGraphConfig.adaptGraphs(flightLog, graphConfig, switchWorkspace == true);
 
         prefs.set('graphConfig', graphConfig);
     }
@@ -1013,7 +1013,7 @@ function BlackboxLogViewer() {
         workspaceSelection.setWorkspaces(newWorkspaces)
         workspaceSelection.setActiveWorkspace(newActiveId)
         if (flightLog && newWorkspaces[newActiveId] && newWorkspaces[newActiveId].graphConfig) {
-           newGraphConfig(newWorkspaces[newActiveId].graphConfig);
+           newGraphConfig(newWorkspaces[newActiveId].graphConfig, true);
            document.getElementById("legend_title").textContent = newWorkspaces[newActiveId].title
         }
     }
