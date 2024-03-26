016bebc08e66e1c6aec7be41379da39df5dc6d60
Tomas Chmelevskij <t.chmelevskij@gmail.com>
2024-03-26 20:59:26 +0100

    feat: add offline pwa files

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,43 @@
+import { VitePWA } from 'vite-plugin-pwa';
+import pkg from './package.json';
+
 /** @type {import('vite').UserConfig} */
 export default { 
     build: { 
         sourcemap: true,
-    }
+    },
+    plugins: [
+        VitePWA({
+            registerType: 'autoUpdate',
+            workbox: {
+                globPatterns: ['**/*.{js,css,html,ico,png,svg,json,mcm}'],
+                // 5MB
+                maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
+            },
+            includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+            manifest: {
+                name: pkg.name,
+                short_name: 'BBE',
+                description: pkg.description,
+                theme_color: '#ffffff',
+                icons: [
+                    {
+                        src: '/images/pwa/bf_icon_128.png',
+                        sizes: '128x128',
+                        type: 'image/png',
+                    },
+                    {
+                        src: '/images/pwa/bf_icon_192.png',
+                        sizes: '192x192',
+                        type: 'image/png',
+                    },
+                    {
+                        src: '/images/pwa/bf_icon_256.png',
+                        sizes: '256x256',
+                        type: 'image/png',
+                    },
+                ],
+            }
+        })
+    ],
 }
