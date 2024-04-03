7071a451a2afad7a7ba922f4247e235b5f79f096
Vladimir Demidov <demvlad2@yandex.ru>
2024-04-03 21:45:42 +0300

    Update src/flightlog.js
    Code style improvement
    
    Co-authored-by: Mark Haslinghuis <mark@numloq.nl>

--- js/flightlog.js
+++ js/flightlog.js
@@ -82,7 +82,7 @@ function FlightLog(logData) {
     this.getStats = function(logIndex) {
         let rawStats = getRawStats(logIndex);
 
-        if (rawStats.field == undefined) {
+        if (rawStats.field === undefined) {
             rawStats.field = [];
             for (let i = 0;  i < rawStats.frame.I.field.length; ++i) {
                 rawStats.field[i] = {
