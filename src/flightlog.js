d8a5e742c3ef0bb31e0f366ee661bdfb6632b054
Vladimir Demidov <demvlad2@yandex.ru>
2024-04-03 21:42:18 +0300

    Update src/flightlog.js
    Code style improvement
    
    Co-authored-by: Mark Haslinghuis <mark@numloq.nl>

--- src/flightlog.js
+++ src/flightlog.js
@@ -1082,8 +1082,7 @@ export function FlightLog(logData) {
         if (fieldStat) {
             min = Math.min(min, fieldStat.min);
             max = Math.max(max, fieldStat.max);
-        }
-        else {
+        } else {
             const mm = this.getMinMaxForFieldDuringTimeInterval(field_name, this.getMinTime(), this.getMaxTime());
             if (mm != undefined) {
                 min = Math.min(mm.min, min);
