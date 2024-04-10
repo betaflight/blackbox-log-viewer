82bf3842e62e4131a9467d1a8937ff11af004afe
demvlad <demvlad2@yandex.ru>
2024-04-10 10:01:55 +0300

    Improvement: The changes of current workspace can store by Shift+S key

--- src/main.js
+++ src/main.js
@@ -1017,6 +1017,7 @@ function BlackboxLogViewer() {
            newGraphConfig(newWorkspaces[newActiveId].graphConfig);
            document.getElementById("legend_title").textContent = newWorkspaces[newActiveId].title
         }
+        activeWorkspace = newActiveId;
     }
 
     // Save current config
@@ -1989,6 +1990,8 @@ function BlackboxLogViewer() {
                                 e.preventDefault();
                             } else if (e.altKey) {
                                 makeScreenshot();
+                            } else if (e.shiftKey) {
+                                onSaveWorkspace(activeWorkspace, workspaceGraphConfigs[activeWorkspace].title);
                             }
                         } catch(e) {
                             console.log('Smoothing override toggle feature not functioning');
