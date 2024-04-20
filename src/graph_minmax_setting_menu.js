3d7e33eeb1f64ece0f922979dae7b70548344a75
demvlad <demvlad2@yandex.ru>
2024-04-20 12:39:39 +0300

    resolved issue of unworked 'As this one' extended submenu

--- src/graph_minmax_setting_menu.js
+++ src/graph_minmax_setting_menu.js
@@ -511,6 +511,7 @@ export function showMinMaxSetupContextMenu(menu_pos_x, menu_pos_y, selected_fiel
 
         elem = $('<div class="topBorder iconDiv">&#9668;SET MIN-MAX VALUES</div>');
         elem.click(function () {
+            ApplySelectedCurveMinMaxToOtherSelectedCurves();
             hideMenu(sub_menu);
             ActivateMainMenu(main_menu);
             $('.back-main-menu').addClass('menu-button');
