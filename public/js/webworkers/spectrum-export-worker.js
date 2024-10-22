2ba1a0ad91384619c525f260966bb78e9641bf62
demvlad <demvlad2@yandex.ru>
2024-10-22 13:23:02 +0300

    The 4 spaces tabs are replaced to 2 space tab

--- public/js/webworkers/spectrum-export-worker.js
+++ public/js/webworkers/spectrum-export-worker.js
@@ -1,16 +1,16 @@
 importScripts("/js/lodash.min.js");
 
 onmessage = function(event) {
-    const columnDelimiter = event.data.opts.columnDelimiter;
-    const fftOutput = event.data.fftOutput;
-    const spectrumDataLength = fftOutput.length / 2;
-    const frequencyStep = 0.5 * event.data.blackBoxRate / spectrumDataLength;
-    
-    let outText = "freq" + columnDelimiter + "value" + "\n"
-    for (let index = 0; index < spectrumDataLength; index += 10) {
-      const frequency = frequencyStep * index;
-      outText += frequency.toString() + columnDelimiter + fftOutput[index].toString() + "\n";
-    }
-    
-    postMessage(outText);
+  const columnDelimiter = event.data.opts.columnDelimiter;
+  const fftOutput = event.data.fftOutput;
+  const spectrumDataLength = fftOutput.length / 2;
+  const frequencyStep = 0.5 * event.data.blackBoxRate / spectrumDataLength;
+
+  let outText = "freq" + columnDelimiter + "value" + "\n"
+  for (let index = 0; index < spectrumDataLength; index += 10) {
+    const frequency = frequencyStep * index;
+    outText += frequency.toString() + columnDelimiter + fftOutput[index].toString() + "\n";
+  }
+
+  postMessage(outText);
 };
