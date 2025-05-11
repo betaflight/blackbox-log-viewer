3a82b42d3e2cf79b853be679b874e932127d9002
demvlad <demvlad2@yandex.ru>
2025-05-11 14:14:49 +0300

    Added PSD values label at mouse position cursor by Shift key

--- src/graph_spectrum_calc.js
+++ src/graph_spectrum_calc.js
@@ -704,7 +704,6 @@ GraphSpectrumCalc._psd  = function(samples, pointsPerSegment, overlapCount, scal
     };
 };
 
-
 /**
  * Compute FFT for samples segments by lenghts as pointsPerSegment with overlapCount overlap points count
  */
