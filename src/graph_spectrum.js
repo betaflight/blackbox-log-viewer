6f9c1ffc51c69b96b57d68ce1da0517b30a0a3f0
demvlad <demvlad2@yandex.ru>
2025-05-29 17:07:37 +0300

    Resolved missing trailing coma issues

--- src/graph_spectrum.js
+++ src/graph_spectrum.js
@@ -260,7 +260,7 @@ export function FlightLogAnalyser(flightLog, canvas, analyserCanvas) {
           const lowLevel = analyserLowLevelPSD.val();
           GraphSpectrumPlot.setLowLevelPSD(lowLevel);
           that.refresh();
-        })
+        }),
       ).dblclick(function () {
         $(this).val(analyserMinPSD.val()).trigger("input");
       }).val(analyserMinPSD.val());
@@ -312,7 +312,7 @@ export function FlightLogAnalyser(flightLog, canvas, analyserCanvas) {
         );
         $("#analyserMinPSDLabel").toggleClass(
           "onlyFullScreenException",
-          !psdHeatMapSelected
+          !psdHeatMapSelected,
         );
         $("#analyserLowLevelPSDLabel").toggleClass(
           "onlyFullScreenException",
