7104be82a74288431e78f18978f64e33d1322a52
demvlad <demvlad2@yandex.ru>
2025-07-16 18:33:36 +0300

    Code issues improvement

--- src/graph_imported_curves.js
+++ src/graph_imported_curves.js
@@ -12,57 +12,57 @@ export function ImportedCurves(curvesChanged) {
   };
 
   this.importCurvesFromCSV = function(files) {
-      let importsLeft = maxImportCount - this._curvesData.length;
+    let importsLeft = maxImportCount - this._curvesData.length;
 
-      for (const file of files) {
-        if (importsLeft-- == 0) {
-          break;
-        }
-        const reader = new FileReader();
-        reader.onload = function (e) {
-          try {
-            const stringRows = e.target.result.split("\n");
-
-            const header = stringRows[0].split(",");
-            if (header.length != 2 || header[0] != "x" || header[1] != "y") {
-              throw new SyntaxError("Wrong curves CSV data format");
-            }
-
-            stringRows.shift();
-            //remove bad last row
-            if (stringRows.at(-1) == "") {
-              stringRows.pop();
-            }
-
-            const curvesData = stringRows.map( function(row) {
-              const data = row.split(","),
-                    x = parseFloat(data[0]),
-                    y = parseFloat(data[1]);
-              _that.minX = Math.min(x, _that.minX);
-              _that.maxX = Math.max(x, _that.maxX);
-              _that.minY = Math.min(y, _that.minY);
-              _that.maxY = Math.max(y, _that.maxY);
-              return {
-                x: x,
-                y: y,
-              };
-            });
-
-            const curve = {
-              name: file.name.split('.')[0],
-              points: curvesData,
-            };
-            _that._curvesData.push(curve);
-            curvesChanged();
-          } catch (e) {
-            alert('Curves data import error: ' + e.message);
-            return;
-          }
-        };
-
-        reader.readAsText(file);
+    for (const file of files) {
+      if (importsLeft-- == 0) {
+        break;
       }
-    };
+      const reader = new FileReader();
+      reader.onload = function (e) {
+        try {
+          const stringRows = e.target.result.split("\n");
+
+          const header = stringRows[0].split(",");
+          if (header.length != 2 || header[0] != "x" || header[1] != "y") {
+            throw new SyntaxError("Wrong curves CSV data format");
+          }
+
+          stringRows.shift();
+          //remove bad last row
+          if (stringRows.at(-1) == "") {
+            stringRows.pop();
+          }
+
+          const curvesData = stringRows.map( function(row) {
+            const data = row.split(","),
+                  x = parseFloat(data[0]),
+                  y = parseFloat(data[1]);
+            _that.minX = Math.min(x, _that.minX);
+            _that.maxX = Math.max(x, _that.maxX);
+            _that.minY = Math.min(y, _that.minY);
+            _that.maxY = Math.max(y, _that.maxY);
+            return {
+              x: x,
+              y: y,
+            };
+          });
+
+          const curve = {
+            name: file.name.split('.')[0],
+            points: curvesData,
+          };
+          _that._curvesData.push(curve);
+          curvesChanged();
+        } catch (e) {
+          alert('Curves data import error: ' + e.message);
+          return;
+        }
+      };
+
+      reader.readAsText(file);
+    }
+  };
 
   this.addCurve = function(points, name) {
     this._curvesData.push({
@@ -76,7 +76,7 @@ export function ImportedCurves(curvesChanged) {
       this.maxY = Math.max(point.y, _that.maxY);
     }
     curvesChanged();
-  }
+  };
 
   this.isNewCurve = function(name) {
     for (const curve of this._curvesData) {
@@ -85,7 +85,7 @@ export function ImportedCurves(curvesChanged) {
       }
     }
     return true;
-  }
+  };
 
   this.removeCurves = function() {
     this._curvesData.length = 0;
