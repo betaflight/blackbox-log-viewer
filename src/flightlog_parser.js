f1041ffa8ac330bf9b17b79476d57d116c1e815b
Mark Haslinghuis <mark@numloq.nl>
2025-09-23 13:21:43 +0200

    Split parsing

--- src/flightlog_parser.js
+++ src/flightlog_parser.js
@@ -960,10 +960,9 @@ export function FlightLogParser(logData) {
             $("html").removeClass("isINAV");
           }
 
-          that.sysConfig.firmware = parseFloat(`${matches[2]}.${matches[3]}`);
+          that.sysConfig.firmware = `${parseInt(matches[2])}.${parseInt(matches[3])}`;
           that.sysConfig.firmwarePatch = matches[5] != null ? parseInt(matches[5]) : "0";
           that.sysConfig.firmwareVersion = `${that.sysConfig.firmware}.${that.sysConfig.firmwarePatch}`;
-          console.log(`Detected firmware version ${that.sysConfig.firmwareVersion}`);
         } else {
           /*
            * Try to detect INAV
