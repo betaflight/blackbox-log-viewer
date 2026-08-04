bebacf32882f193ed4604b90868f5be47ceb2615
BP Hoops <benjaminpaulhooper@gmail.com>
2026-08-04 13:41:15 +0100

    sizes

--- src/user_settings_data.js
+++ src/user_settings_data.js
@@ -70,7 +70,7 @@ export const defaultUserSettings = {
   overdrawSpectrumType: 0,
   psdHeatmapMin: -40,
   psdHeatmapMax: 10,
-  craft: { left: "15%", top: "48%", size: "40%" },
+  craft: { left: "10%", top: "20%", size: "35%" },
   sticks: { left: "75%", top: "20%", size: "30%" },
   analyser: { left: "2%", top: "60%", size: "35%" },
   analyser_legend: { left: "88%", top: "7%", width: "10%" },
